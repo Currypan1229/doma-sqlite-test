645390061d6e831f6d48116133648b8c5f0a45bb
Currypan1229 <abcqqqwpvp@gmail.com>
2024-03-31 05:32:02 +0900

    一部コードの修正

--- src/main/java/dev/currypan/test/doma/IUnknownColumnHandler.java
+++ src/main/java/dev/currypan/test/doma/IUnknownColumnHandler.java
@@ -7,10 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IUnknownColumnHandler implements UnknownColumnHandler {
-    private final Logger log = LoggerFactory.getLogger(IUnknownColumnHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(IUnknownColumnHandler.class);
 
     @Override
     public void handle(final Query query, final EntityType<?> entityType, final String unknownColumnName) {
-        this.log.error("Unknown column was found  | Class:{}  | Method:{}  | Column:{}", query.getClassName(), query.getMethodName(), unknownColumnName);
+        LOGGER.error("Unknown column was found  | Class:{}  | Method:{}  | Column:{}", query.getClassName(), query.getMethodName(), unknownColumnName);
     }
 }
